e0f35f3b4afc6d45d147c371bb0f6c190bb78400
Kamaluddin Osman Musa Osman <kamaluddin.osman@gmail.com>
2020-11-26 17:29:38 +0200

    Adding node debug

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Joi       = require('Joi');
 const express   = require('express');
 const morgan    = require('morgan');
 const config    = require('config');
+const debug     = require('debug')('app:startup');
 const customMiddleware = require('./customMiddleware');
 const app = express();
 app.use(express.json());
@@ -10,14 +11,14 @@ app.use(customMiddleware);
 app.use(express.static('public'));
 
 // Configration
-console.log("Application Name: " + config.get('name'));
-console.log("Mail Server: " + config.get('mail.host'));
-console.log("Mail Password: " + config.get('mail.password'));
+debug("Application Name: " + config.get('name'));
+debug("Mail Server: " + config.get('mail.host'));
+debug("Mail Password: " + config.get('mail.password'));
 
 // Set morgan to works only in development environment
 if (app.get('env') === 'development'){
     app.use(morgan('tiny'));
-    console.log(`NODE_ENV: ${app.get('env')}, and Morgan is enabled`);
+    debug(`NODE_ENV: ${app.get('env')}, and Morgan is enabled`);
 }
 const items = [
     { id: 1, name: "element 1"},
